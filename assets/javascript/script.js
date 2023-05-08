0bae308e524c2c1d9bb11c6f4c9fb2ca568442e9
jurba1997 <jurba2011@gmail.com>
2023-05-08 16:56:55 +0000

    write the js code to add a task when the add task button is clicked

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,6 +3,7 @@ window.addEventListener('load', () =>{
     const input = document.querySelector('#task-input')
     const list_el = document.querySelector('#tasks')
 
+    
     form.addEventListener('submit', (e) =>{
         e.preventDefault()
 
@@ -12,5 +13,16 @@ window.addEventListener('load', () =>{
         alert("Please fill out the field.");
         return;
     }
+
+    const task_el = document.createElement("div");
+    task_el.classList.add("task");
+    
+    const task_content_el = document.createElement("div");
+    task_content_el.classList.add("content");
+    task_content_el.innerText = task;
+
+    task_el.appendChild(task_content_el);
+
+    list_el.appendChild(task_el);
     })
 })
